0de577f628257c9948686d3838f084b9224e58ba
ike tohru <rokujyouhitomajp@gmail.com>
2012-04-17 23:30:21 +0900

    commit

---
+++ src/r12a/events/EventTarget.js
@@ -0,0 +1,21 @@
+goog.provide('r12a.events.EventTarget');
+
+goog.require('r12a.Disposable');
+
+
+/**
+ *  @constructor
+ *  @extends {r12a.Disposable}
+ */
+r12a.events.EventTarget = function() {
+  r12a.Disposable.call(this);
+};
+goog.inherits(r12a.events.EventTarget, r12a.Disposable);
+
+
+/**
+ * 
+ */
+r12a.events.EventTarget.prototype.xxx = function() {
+  //
+};
